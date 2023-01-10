1e5f718339fe9f69421f226fcbe8b7f879b8e8b2
MinhwanO <107867881+MinhwanO@users.noreply.github.com>
2023-01-10 15:18:50 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -57,7 +57,7 @@ app.post('/api/users/login', (req, res) => {
         user.generateToken((err, user) => {
             if(err) return res.status(400).send(err);
 
-            res.cookie("x_auth", user.token)
+            res.cookie("x_auth", user.token, {sameSite: 'none', secure: true, httpOnly: ture})
             .status(200)
             .json({
                 loginSuccess: true,
